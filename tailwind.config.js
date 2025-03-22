bf6364ef2ced47ded825badd3653adcd4d445394
wada <115588841+wadagz@users.noreply.github.com>
2025-03-21 18:24:58 -0600

    build: cambiado a tailwind css.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,34 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.vue',
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: {
+                    50: '#ebeff3',
+                    100: '#d6dfe7',
+                    200: '#adbecf',
+                    300: '#849eb8',
+                    400: '#5b7da0',
+                    500: '#325d88',
+                    600: '#5b7da0',
+                    700: '#386999',
+                    800: '#325d88',
+                    900: '#264666',
+                    950: '#192f44',
+                },
+                secondary: '#8e8c84',
+                success: '#93c54b',
+                info: '#29abe0',
+                warning: '#f47c3c',
+                danger: '#d9534f',
+                light: '#f8f5f0',
             },
         },
     },
