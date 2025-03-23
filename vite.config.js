1691205f70974bed01837883c0361528d83fccc6
wada <115588841+wadagz@users.noreply.github.com>
2025-03-22 18:11:27 -0600

    build: agregado ziggy-js para poder usar rutas dentro de las vistas de Vue.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+import path from 'path';
+
 export default defineConfig({
+    resolve: {
+        alias: {
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist/index.esm.js'),
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
