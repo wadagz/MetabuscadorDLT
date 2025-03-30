3090a7b6227370560bdc1b693132d0ddb5d3b779
wada <115588841+wadagz@users.noreply.github.com>
2025-03-30 00:40:27 -0600

    style: modificado altura de contenedores en Hospedajes/Index.

--- resources/js/Pages/HospedajesDestino/Index.vue
+++ resources/js/Pages/HospedajesDestino/Index.vue
@@ -19,16 +19,23 @@ const props = defineProps({
     :fechaRegreso="fechaRegreso"
     :puntoPartida="puntoPartida"
 />
+
 <div>Aquí van los filtros para buscar hospedajes.</div>
 
-<div class="container mx-auto grid grid-cols-2 gap-4 h-1/2">
+<div class="container mx-auto mb-2">
+    <h2 class="text-xl">
+        Hospedajes encontrados en {{ destino }}
+    </h2>
+</div>
+
+<div class="container mx-auto grid grid-cols-2 gap-4">
     <!-- Lista de hospedajes -->
-    <div class="bg-gray-200 rounded-lg overflow-y-scroll h-1/6">
+    <div class="bg-gray-200 rounded-lg border border-gray-400 shadow-md overflow-y-scroll h-[44rem]">
         <div v-for="hospedaje in hospedajes" :key="hospedaje.id">
             <CardHospedaje :hospedaje="hospedaje" />
         </div>
     </div>
-    <div class="bg-gray-200 rounded-lg h-1/6">A la derecha un mapa donde se muestren las ubicaciones de los hospedajes.</div>
+    <div class="bg-gray-200 rounded-lg border border-gray-400 shadow-md h-[44rem]">A la derecha un mapa donde se muestren las ubicaciones de los hospedajes.</div>
 </div>
 
 
