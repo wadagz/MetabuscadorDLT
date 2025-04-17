cbd8358f8629372d6603e8deaf325187f6b57179
wada <115588841+wadagz@users.noreply.github.com>
2025-04-16 18:48:19 -0600

    feat: ResenaCard component.

---
+++ resources/js/Pages/Hospedaje/Partials/ResenaCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Icon } from '@iconify/vue/dist/iconify.js';
+</script>
+
+<template>
+<div class="card bg-light m-4 px-2 border border-gray-300 rounded-md">
+    <div class="flex items-center justify-between">
+        <div class="text-lg">
+            <b>usuario</b>
+        </div>
+        <div>
+            <Icon icon="material-symbols-light:star-outline-rounded" :width="30" style="display: inline;"/>
+            <Icon icon="material-symbols-light:star-outline-rounded" :width="30" style="display: inline;"/>
+            <Icon icon="material-symbols-light:star-outline-rounded" :width="30" style="display: inline;"/>
+            <Icon icon="material-symbols-light:star-outline-rounded" :width="30" style="display: inline;"/>
+            <Icon icon="material-symbols-light:star-outline-rounded" :width="30" style="display: inline;"/>
+        </div>
+    </div>
+
+    <hr class="my-1">
+
+    <div class="pb-2">
+        comentario
+    </div>
+</div>
+</template>
