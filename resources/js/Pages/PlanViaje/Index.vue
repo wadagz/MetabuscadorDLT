40905d0c27916664393c1c6f7b4d2eb780fb83b8
wada <115588841+wadagz@users.noreply.github.com>
2025-04-27 16:52:51 -0600

    feat: props para vista Planificacion de Viaje.

--- resources/js/Pages/PlanViaje/Index.vue
+++ resources/js/Pages/PlanViaje/Index.vue
@@ -1,19 +1,21 @@
 <script setup>
 
 const props = defineProps({
-    destino: String,
-    fechaPartida: Number,
-    fechaRegreso: Number,
+    destino: Object,
+    fechaPartida: String,
+    fechaRegreso: String,
     puntoPartida: String,
+    caminos: Object,
 })
 </script>
 
 <template>
-    {{ destino }}
-    {{ fechaPartida }}
-    {{ fechaRegreso }}
-    {{ puntoPartida }}
 <div class="card container mx-auto pt-4 pb-10">
     <h1 class="text-3xl mb-4">Planificación de Viaje</h1>
+</div>
+<div>
+    <div v-for="camino in caminos">
+        {{ camino }}
+    </div>
 </div>
 </template>
